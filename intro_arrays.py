4c9a64edd7fbab5e002bb7861c4e65a11af8dccc
João Freitas <a25077@esenviseu.net>
2024-12-11 11:07:03 +0000

    Introdução aos arrays

---
+++ intro_arrays.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+numeros = np.array([10,14,-5,33,120])
+
+for i in range(5):
+    print(numeros[i])
+
+for i in range(5):
+    numeros[i] = 0
+
+array_0d = np.array(40)
+
+print(type(numeros))
+print(type(array_0d))
+print(array_0d)
+
+print(numeros.ndim)# nº de dimensões do array
+
+for i in range(5):
+    print(numeros[i])
+
+for x in numeros:
+    print(x)
+
+for x in range(len(numeros)):
+    print(numeros[x])
+
+#criar um array vazio
+vazio = np.empty(10)
+print(vazio)
+
+#criar um array com zeros
+zeros = np.zeros(10)
+print(zeros)
---
+++ notas.py
@@ -0,0 +1,8 @@
+"""
+Programa para ler as notas de 10 alunos, calcular e mostrar a média das notas e por fim mostrar
+as notas que são superiores à média
+"""
+
+import numpy as np
+#definir o array para as notas
+notas = np.zeros(10)
